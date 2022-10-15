f41b2c8710eed64e5899203cf3768ffd4c0d667e
Sumant Mishra <107248846+sumantmishra1920@users.noreply.github.com>
2022-10-15 20:49:33 +0530

    Delete SimpleHelloWorld directory

--- SimpleHelloWorld/src/Sum/Sum.java
+++
@@ -1,9 +0,0 @@
-package Sum;
-public class Sum
-{
-	public static void main(String args[])
-	{
-		System.out.println("Hello");
-	}
-
-}
--- SimpleHelloWorld/src/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * @author suman
- *
- */
-module SimpleHelloWorld {
-}
